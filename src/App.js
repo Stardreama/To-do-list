471e7240a5614a44ae1f6757ea727e015196253c
Stardreama <1663302572a@gmail.com>
2024-04-07 22:02:22 +0800

    finish input with display

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React from 'react';
-import { DatePicker, Space, Input,Row,Col } from 'antd';
-const onChange = (date, dateString) => {
-  console.log(date, dateString);
-};
+import React, { useState } from 'react';
+import { DatePicker, Input, Row, Col, Button, Flex } from 'antd';
 function App() {
-  const all_style={
-    width:500
-  }
   const titleStyle = {
     fontSize: 30,
     textAlign: "center",
   }
-  const contentStyle = {
-    textAlign: "center",
+  const inputAdd = {
+    width: 400
+  }
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const handleDateChange = (date, dateString) => {
+    console.log(dateString);
+    setDate(dateString)
+  };
+  const getName = (name) => {
+    setName(name)
+  }
+  const handleAdd = () => {
+    if (name && date) {
+      console.log(name, date);
+    }
+    else {
+      console.log("11111");
+    }
   }
   return (
-    <div >
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1 style={titleStyle}> To Do List</h1>
-      <div style={contentStyle}>
-        <div >
-          <Input placeholder="输入任务名称" style={{ width: 500 }} />
-        </div>
-        <div>
-          <Space direction="vertical">
-            <DatePicker onChange={onChange} placeholder="选择截止时间" />
-          </Space>
-        </div>
+      <div>
+        <Row>
+          <Col style={inputAdd}>
+            <Input placeholder="输入任务名称" onChange={(e) => getName(e.target.value)} />
+          </Col>
+        </Row>
+        <Row >
+          <Col >
+            <DatePicker onChange={handleDateChange} placeholder="选择截止时间" />
+            <Button type="primary" name={name} date={date} onClick={handleAdd}>添加</Button>
+          </Col>
+        </Row>
       </div>
     </div>
   );
 }
-
 export default App;
