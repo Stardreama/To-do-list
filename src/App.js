bebf08e565a18c42da6d34bcaf7df587b4974d78
Stardreama <1663302572a@gmail.com>
2024-04-08 00:08:27 +0800

    without delete the textarea,finish add and delete assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { DatePicker, Input, Row, Col, Button, Flex } from 'antd';
+import React, { useState, useRef } from 'react';
+import { DatePicker, Input, Row, Col, Button, Typography, Checkbox } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import { v4 as uuidv4 } from 'uuid'
 function App() {
   const titleStyle = {
     fontSize: 30,
@@ -8,6 +10,8 @@ function App() {
   const inputAdd = {
     width: 400
   }
+  const { Text, Link } = Typography;
+  const [toDoList, setToDoList] = useState([])
   const [name, setName] = useState('')
   const [date, setDate] = useState('')
   const handleDateChange = (date, dateString) => {
@@ -20,10 +24,24 @@ function App() {
   const handleAdd = () => {
     if (name && date) {
       console.log(name, date);
+      setToDoList([
+        ...toDoList,
+        {
+          toDoName: name,
+          toDoTime: date,
+          id: uuidv4()
+        }
+      ])
     }
     else {
       console.log("11111");
     }
+  }
+  const handleFinish = (e) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
+  const handleDelete = (id) => {
+    setToDoList(toDoList.filter(item => item.id !== id))
   }
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -31,7 +49,7 @@ function App() {
       <div>
         <Row>
           <Col style={inputAdd}>
-            <Input placeholder="输入任务名称" onChange={(e) => getName(e.target.value)} />
+            <Input placeholder="输入任务名称"  onChange={(e) => getName(e.target.value)} />
           </Col>
         </Row>
         <Row >
@@ -41,6 +59,22 @@ function App() {
           </Col>
         </Row>
       </div>
+      {toDoList.map(item => (
+        <div style={inputAdd}>
+          <Row key={item.id} style={{ display: 'flex', alignItems: 'center' }}>
+            <Col style={{ marginRight: 250 }}>
+              <div>{item.toDoName}</div>
+              <div>{item.toDoTime}</div>
+            </Col>
+            <Col style={{ marginRight: 30 }}>
+              <Checkbox onChange={handleFinish}></Checkbox>
+
+            </Col >
+            <Col><DeleteOutlined onClick={() => handleDelete(item.id)} /></Col>
+          </Row>
+        </div>
+      ))}
+
     </div>
   );
 }
