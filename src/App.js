2db9bd7e10d80deb1d3706cb777167d23cbdbb2b
Stardreama <1663302572a@gmail.com>
2024-04-08 20:18:51 +0800

    finish with the bug the same day is red

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react';
-import { DatePicker, Input, Row, Col, Button, Typography, Checkbox } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
+import { DatePicker, Input, Row, Col, Button, Typography, Checkbox, message } from 'antd';
+import { DeleteOutlined, InboxOutlined } from '@ant-design/icons';
 import { v4 as uuidv4 } from 'uuid'
+import moment from 'moment'
 function App() {
   const titleStyle = {
     fontSize: 30,
@@ -10,72 +11,111 @@ function App() {
   const inputAdd = {
     width: 400
   }
+  useEffect(() => {
+    document.title = "Todo List";
+  }, []);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [flag, setFlag] = useState(0)
   const { Text, Link } = Typography;
   const [toDoList, setToDoList] = useState([])
   const [name, setName] = useState('')
   const [date, setDate] = useState('')
+  const [objectDateNow, setObjectDateNow] = useState(new Date(moment().format('YYYY-MM-DD')))
+  // const [objectDateTarget,setObjectDateTarget]=useState('')
+  const [keyValue,setKeyValue]=useState('')
   const handleDateChange = (date, dateString) => {
     console.log(dateString);
     setDate(dateString)
   };
+  const error = () => {
+    messageApi.open({
+      type: 'error',
+      content: '请输入任务名称和截止时间',
+    });
+  };
+
   const getName = (name) => {
     setName(name)
   }
   const handleAdd = () => {
     if (name && date) {
       console.log(name, date);
+      const targetTime = moment(date).toDate();
       setToDoList([
         ...toDoList,
         {
           toDoName: name,
           toDoTime: date,
-          id: uuidv4()
+          id: uuidv4(),
+          isFinished: false,
+          targetTime: targetTime
         }
       ])
+      setKeyValue(new Date())
+      setFlag(flag + 1)
+      setName('')
+      setDate('')
     }
     else {
+      error()
       console.log("11111");
     }
   }
-  const handleFinish = (e) => {
-    console.log(`checked = ${e.target.checked}`);
+  const handleFinish = (id) => {
+    setToDoList(toDoList.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isFinished: !item.isFinished
+        }
+      }
+      else {
+        return item
+      }
+    }))
   };
   const handleDelete = (id) => {
     setToDoList(toDoList.filter(item => item.id !== id))
+    setFlag(flag - 1)
   }
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      {contextHolder}
       <h1 style={titleStyle}> To Do List</h1>
       <div>
         <Row>
           <Col style={inputAdd}>
-            <Input placeholder="输入任务名称"  onChange={(e) => getName(e.target.value)} />
+            <Input placeholder="输入任务名称" value={name} onChange={(e) => getName(e.target.value)} />
           </Col>
         </Row>
         <Row >
           <Col >
-            <DatePicker onChange={handleDateChange} placeholder="选择截止时间" />
+            <DatePicker onChange={handleDateChange} key={keyValue} placeholder="选择截止时间" />
             <Button type="primary" name={name} date={date} onClick={handleAdd}>添加</Button>
           </Col>
         </Row>
+        {flag > 0 || (<div style={{ marginLeft: 150, marginTop: 100 }}>
+          <InboxOutlined style={{ fontSize: 50, color: '#e6e6e6' }} />
+          <div style={{ color: '#e6e6e6' }}> No Data</div>
+        </div>)}
       </div>
       {toDoList.map(item => (
         <div style={inputAdd}>
           <Row key={item.id} style={{ display: 'flex', alignItems: 'center' }}>
             <Col style={{ marginRight: 250 }}>
-              <div>{item.toDoName}</div>
-              <div>{item.toDoTime}</div>
+              {item.isFinished ? (<div style={objectDateNow > item.targetTime ? { color: 'red', textDecoration: 'line-through' } : { color: 'black', textDecoration: 'line-through' }}>{item.toDoName}</div>) : (<div style={objectDateNow > item.targetTime ? { color: 'red' } : { color: 'black' }}>{item.toDoName}</div>)}
+              {item.isFinished ? (<div style={objectDateNow > item.targetTime ? { color: 'red', textDecoration: 'line-through' } : { color: 'black', textDecoration: 'line-through' }}>{item.toDoTime}</div>) : (<div style={objectDateNow > item.targetTime ? { color: 'red' } : { color: 'black' }}>{item.toDoTime}</div>)}
+              {objectDateNow > item.targetTime ? (console.log('red', '当前时间为', objectDateNow, '目标', item.targetTime)) : console.log('black', '当前时间为', objectDateNow, '目标', item.targetTime)}
             </Col>
             <Col style={{ marginRight: 30 }}>
-              <Checkbox onChange={handleFinish}></Checkbox>
-
+              <Checkbox onChange={() => handleFinish(item.id)}></Checkbox>
             </Col >
             <Col><DeleteOutlined onClick={() => handleDelete(item.id)} /></Col>
           </Row>
         </div>
-      ))}
-
-    </div>
+      ))
+      }
+    </div >
   );
 }
 export default App;
